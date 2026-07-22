2ed75f0f43c5f7f7c9c88dc45f7e791664e76864
Luis Felipe Arellano Roque <luisfelipearellano2004@gmail.com>
2026-07-22 22:03:25 +0000

    🔧 FIX: Configuración para Vercel

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true,
     domains: ['ssgwsajkzyxqgeeagwxc.supabase.co'],
   },
-  trailingSlash: true,
-  distDir: 'out',
-  // Configurar para GitHub Pages - SIN basePath
-  assetPrefix: '',
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
